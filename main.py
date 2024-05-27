2c2ca6479d1e499204571de6bd002a0848310fa2
david15tonon <82893290+david15tonon@users.noreply.github.com>
2024-05-27 01:37:09 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from medical_data_visualizer import draw_cat_plot, draw_heat_map
+
+# Import the tests
+import test_module
+
+# Run the tests
+if __name__ == "__main__":
+    draw_cat_plot()
+    draw_heat_map()
+    test_module.run_tests()
